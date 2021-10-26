c3c2a31f3afdd46a4cc797063042f0e9e25a6a33
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-10-26 12:32:18 -0300

    refactor: remodeling of update-game-service

--- src/services/update-game-service.js
+++ src/services/update-game-service.js
@@ -1,11 +1,19 @@
 const { GameModel } = require('../database/schemas/gameSchema')
 
+async function refactorPlatformFieldAndUpdate(selectedGame, platform) {
+    const newPlatform = { ...selectedGame.platform, ...platform }
+
+    selectedGame.platform = newPlatform
+
+    await selectedGame.save()
+
+    return selectedGame
+}
+
 module.exports = { 
-    handle: async function (_id, title, launchYear, developer, platform1) {
+    handle: async function (_id, title, launchYear, developer, platform) {
         const selectedGame = await GameModel.findByIdAndUpdate(_id, { title, launchYear, developer })
-        const x = { ...selectedGame.platform, ...platform1 }
-        selectedGame.platform = x
-        await selectedGame.save()
-        return selectedGame
+        
+        return await refactorPlatformFieldAndUpdate(selectedGame, platform)
     }
 }
